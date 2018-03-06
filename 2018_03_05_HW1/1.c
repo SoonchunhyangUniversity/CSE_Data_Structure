0f8bb7ebe983035ff386f1511ab664daa49dc7a5
alstn2468 <alstn2468_@naver.com>
2018-03-06 23:02:42 +0900

    2018-03-06

--- 2018_03_05_HW1/1.c
+++ 2018_03_05_HW1/1.c
@@ -11,6 +11,7 @@
 */
 
 #include <stdio.h>
+#define TRUE 1
 
 int main()
 {
@@ -29,7 +30,7 @@ int main()
 		exit(-1);
 	}
 
-	/* fscanf로 arr에 값 삽입 */
+	/* fscnaf로 arr에 값 삽입 */
 	while (!feof(fp))
 	{
 		fscanf(fp, "%d", &arr[i]);	// 파일에서 정수를 입력받아 배열에 하나씩 넣는다
@@ -59,87 +60,42 @@ int main()
 		}
 	}
 
-	int check;	// 5개씩 출력을 위한 변수 선언
+	int check = 0;	// 5개씩 출력을 위한 변수 선언
 
 	/* 배열 안에 있는 모든 값, 최댓값 최솟값 출력 */
 	for (int i = 0; i < num_count; i++)
 	{
 		printf("%4d\t", arr[i]);   // 배열안의 정수를 4자리씩 차지하게 출력
 
-		if (min_idx == i)	// i의 값이 최솟값 idx와 같으면
-			check = 1;		// check = 1
-
-		if (max_idx == i)	// i의 값이 최댓값 idx와 같으면
-			check = 2;		// check = 2
-
 		if ((i + 1) % 5 == 0 && i != 0)	// 한 줄에 5개씩 출력
 		{
 			printf("\n");
 
-			/* 최솟값 숫자 아래에 문자열 "최솟값" 출력 */
-			if (check == 1)
+			/* 정수아래 최댓값 최솟값 공백 출력을 위한 반복문 */
+			while(TRUE)
 			{
-				for (int l = 0; l < min_idx % 5; l++)
+				if (check == max_idx)		// check 변수가 max_idx값과 같으면
+					printf("최댓값\t");		// 문자열 "최댓값" 출력
+				else if (check == min_idx)	// check 변수가 min_idx값과 같으면
+					printf("최솟값\t");		// 문자열 "최솟값" 출력
+				else						// 둘 다 아니면
+					printf("\t");			// 공백 출력
+
+				check++;	// check += 1과 같음
+
+				if (check % 5 == 0)	// 한줄에 5개씩 출력 check가 5이면
 				{
-					printf("\t");	// tab 이용
+					printf("\n");	// 개행
+					break;			// while문 종료
 				}
-				printf("최솟값");
-				for (int k = 0; k < 5 - (min_idx % 5); k++)
-				{
-					printf("\t");
-				}
-				printf("\n");
-				check = 0;	// 변수 check 초기화
-			}
-			/* 최댓값 숫자 아래에 문자열 "최댓값" 출력 */
-			else if (check == 2)
-			{
-				for (int l = 0; l < max_idx % 5; l++)
-				{
-					printf("\t"); // tab 이용
-				}
-				printf("최댓값");
-				for (int k = 0; k < 5 - (max_idx % 5); k++)
-				{
-					printf("\t");
-				}
-				printf("\n");
-				check = 0;	// 변수 check 초기화
-			}
-			/* 최댓값과 최솟값이 아닌 원소 아래 공백 출력 */
-			else
-			{
-				for (int k = 0; k < 5; k++)
-				{
-					printf("\t");
-				}
-				printf("\n");
 			}
 		}
 	}
 
+	/* max_idx와 min_idx arr[max_idx] arr[min_idx]가 알맞은 값을 가리키는지 확인*/
 	printf("  min idx   = %d max idx   = %d\n  min value = %d max value = %d\n", min_idx, max_idx, arr[min_idx], arr[max_idx]);
 
-	/* 한 줄씩 두 줄  출력 하기
-	for (int m = 0; m < num_count; m++)
-	{
-		printf(" %4d ", arr[m]);
-	}
-		printf("\n");
+	fclose(fp);	// 파일포인터 fp를 닫아준다.
 
-	for (int l = 0; l < num_count; l++)
-	{
-		if (l == max_idx)
-		printf(" 최댓값 ");
-		else if (l == min_idx)
-		printf("최솟값 ");
-		else
-		printf("      ");
-	}
-	printf("\n  min idx   = %d max idx   = %d\n  min value = %d max value = %d\n", min_idx, max_idx, arr[min_idx], arr[max_idx]);
-	*/
-
-	fclose(fp);
-
-	return 0;		// O(n)
+	return 0;	// O(n)
 }
