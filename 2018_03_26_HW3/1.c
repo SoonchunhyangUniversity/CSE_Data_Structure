b96a9ea8b568862b57df29569fb61f92a80136f3
alstn2468 <alstn2468_@naver.com>
2018-03-27 19:25:10 +0900

    2018-03-27

--- 2018_03_26_HW3/1.c
+++ 2018_03_26_HW3/1.c
@@ -1,3 +1,15 @@
+/*
+	*
+	* 작성자 : 김민수
+	* 학번 : 20174444
+	* 학과 : 컴퓨터소프트웨어공학과
+	* 프로그램명 : 03 과제
+	* 개발환경 : Visual Studio 2010
+	* 컴파일러 표준 : C99
+	* 작성일 : 2018 - 03 - 26
+	*
+*/
+
 #include <stdio.h> // 표준입출력 라이브러리 헤더파일 선언
 #include <stdlib.h> // malloc, free함수를 사용하기 위한 헤더파일 선언
 
@@ -85,8 +97,8 @@ int main() // 메인함수 시작
 	/* 행열의 덧셈 연산의 조건 */
 	if (COLS_A == COLS_B && ROWS_A == ROWS_B) // 두 행열의 크기가 같아야함
 	{
-		matrix_sum(Matrix_A, Matrix_B, Matrix_C, COLS_A, ROWS_A);
-		// matrix_sum 함수에 Matrix_A, Matrix_B, Matrix_C, COLS_A, ROWS_A를 파라미터로 전달
+		matrix_sum(Matrix_A, Matrix_B, Matrix_C, COLS_A);
+		// matrix_sum 함수에 Matrix_A, Matrix_B, Matrix_C, COLS를 파라미터로 전달
 		printf("A 행렬과 B 행렬의 합\n\n");
 		print_matrix(Matrix_C, ROWS_A, COLS_A);
 		// print_matrix함수에 Matrix_C, ROWS_A, COLS_A를 파라미터로 전달해 곱연산 결과 출력
@@ -95,72 +107,79 @@ int main() // 메인함수 시작
 	else // 합 연산의 조건에 맞지 않으면
 		printf("MATRIX SIZE ERROR!\n"); // 에러메세지 출력
 
+	/* 행열의 뺄셈 연산의 조건 */
 	if (COLS_A == COLS_B && ROWS_A == ROWS_B)
 	{
-		matrix_sub(Matrix_A, Matrix_B, Matrix_C, COLS_A, COLS_B);
+		matrix_sub(Matrix_A, Matrix_B, Matrix_C, COLS_A); // 두 행열의 크기가 같아야함
+		// matrix_suㅠ 함수에 Matrix_A, Matrix_B, Matrix_C, COLS_A를 파라미터로 전달
 		printf("A 행렬과 B 행렬의 차\n\n");
 		print_matrix(Matrix_C, ROWS_A, COLS_A);
+		// print_matrix함수에 Matrix_C, ROWS_A, COLS_A를 파라미터로 전달해 곱연산 결과 출력
 		printf("\n");
 	}
-	else
-		printf("MATRIX SIZE ERROR!\n");
+	else // 차 연산의 조건에 맞지 않으면
+		printf("MATRIX SIZE ERROR!\n"); // 에러메세지 출력
 
+	/* 동적할당한 배열들의 메모리 해제 */
 	for(int i = 0; i < ROWS_A; i++)
 		free(Matrix_A[i]);
-
+	free(Matrix_A);
 	for (int i = 0; i < ROWS_B; i++)
 		free(Matrix_B[i]);
-
+	free(Matrix_B);
 	for (int i = 0; i < ROWS_B; i++)
 		free(Matrix_C[i]);
+	free(Matrix_C);
 
-	return 0;
+	fclose(fp); // 파일 포인터를 닫아줌
+
+	return 0; // 메인함수 종료
 }
-
+/* 행렬 A와 행렬 B를 곱해서 행렬 C를 만드는 함수 */
 void matrix_mul(int **matrix1, int **matrix2, int **matrix3, int ROWS, int COLS)
-{
-	for (int i = 0; i < ROWS; i++)
+{	// 매개변수로 2차원 배열 3개, 행과 열의 값을 받는다.
+	for (int i = 0; i < ROWS; i++) // ROWS번 반복한다.
 	{
-		for (int j = 0; j < COLS; j++)
+		for (int j = 0; j < COLS; j++) // COLS번 반복한다.
 		{
-			for (int k = 0; k < ROWS; k++)
-				matrix3[i][j] = matrix1[i][k] * matrix2[k][j];
+			for (int k = 0; k < ROWS; k++) // ROWS번 반복한다.
+				matrix3[i][j] = matrix1[i][k] * matrix2[k][j]; // 행렬의 값을 곱한다.
 		}
 	}
 }
-
+/* 행렬 A와 행렬 B를 더해서 행렬 C를 만드는 함수 */
 void matrix_sum(int **matrix1, int **matrix2, int **matrix3, int ROWS)
-{
-	for (int i = 0; i < ROWS; i++)
+{	// 매개변수로 2차원 배열 3개, 열의 값을 받는다.
+	for (int i = 0; i < ROWS; i++) // ROWS번 반복한다.
 	{
-		for (int j = 0; j < ROWS; j++)
+		for (int j = 0; j < ROWS; j++) // ROWS번 반복한다.
 		{
-			matrix3[i][j] = matrix1[i][j] + matrix2[i][j];
+			matrix3[i][j] = matrix1[i][j] + matrix2[i][j]; // 행렬의 값을 더해준다.
 		}
 	}
 }
-
+/* 행렬 A와 행렬 B를 빼서 행렬 C를 만드는 함수 */
 void matrix_sub(int **matrix1, int **matrix2, int **matrix3, int ROWS)
-{
-	for (int i = 0; i < ROWS; i++)
+{	// 매개변수로 2차원 배열 3개, 열의 값을 받는다.
+	for (int i = 0; i < ROWS; i++) // ROWS번 반복한다.
 	{
-		for (int j = 0; j < ROWS; j++)
+		for (int j = 0; j < ROWS; j++) // ROWS번 반복한다.
 		{
-			matrix3[i][j] = matrix1[i][j] - matrix2[i][j];
+			matrix3[i][j] = matrix1[i][j] - matrix2[i][j]; // 행렬의 값을 뺀다.
 		}
 	}
 }
-
+/* 행렬을 출력하는 함수 */
 void print_matrix(int **matrix, int ROWS, int COLS)
-{
-	for (int i = 0; i < ROWS; i++)
+{	// 매개변수로 2차원 배열 1개, 행과 열의 값을 받는다.
+	for (int i = 0; i < ROWS; i++) // ROWS번 반복
 	{
-		for (int j = 0; j < COLS; j++)
+		for (int j = 0; j < COLS; j++) // COL번 반복
 		{
-			if (j == COLS - 1)
-				printf("│ %3d │", matrix[i][j]);
-			else
-				printf("│ %3d ", matrix[i][j]);
+			if (j == COLS - 1) // 줄의 마지막 원소일때
+				printf("│ %3d │", matrix[i][j]); // 뒤에 │를 붙여 출력
+			else // 마지막이 아닌 모든 원소
+				printf("│ %3d ", matrix[i][j]); // 행렬의 값 출력
 		}
 
 		printf("\n");
--- Practice/2018_03_27/3.c
+++ Practice/2018_03_27/3.c
@@ -3,10 +3,10 @@
 	* 작성자 : 김민수
 	* 학번 : 20174444
 	* 학과 : 컴퓨터소프트웨어공학과
-	* 프로그램명 : 행렬의 계산
+	* 프로그램명 : 난수의 생성 및 검색
 	* 개발환경 : Visual Studio 2010
 	* 컴파일러 표준 : C99
-	* 작성일 : 2018 - 03 - 27
+	* 작성일 : 2018 - 03 - 20
 	*
 */
 
@@ -30,7 +30,7 @@ int main() // 메인함수 시작
 	int COLS_C, ROWS_C; // B행렬의 행과 열의 개수를 담을 정수형 변수 선언
 
 	/* 파일 포인터 fp선언 및 data.txt를 읽기모드로 연다. */
-	FILE *fp = fopen("data1.txt", "r");
+	FILE *fp = fopen("data.txt", "r");
 
 	/* 파일 열기에 실패했을 경우 예외처리 */
 	if (fp == NULL)	// 파일이 없다면
@@ -78,8 +78,8 @@ int main() // 메인함수 시작
 				fscanf(fp, "%d", &Matrix_C[i][j]);
 		}
 
-		fscanf(fp, "%s", oper1);
-		fscanf(fp, "%s", oper2);
+		fscanf(fp, "%s", oper1); // 연산1을 oper1에 저장한다.
+		fscanf(fp, "%s", oper2); // 연산2를 oper2에 저장한다.
 	}
 
 	/* 행렬 연산을 진행하기전의 행렬 데이터 출력 */
@@ -96,6 +96,7 @@ int main() // 메인함수 시작
 	// print_matrix함수에 Matrix_C, COLS_C, ROWS_C를 파라미터로 전달
 	printf("\n");
 
+	int check = 0;
 	/* 행렬 계산 조건 판단 모든 행렬의 크기가 같으면 연산 진행 */
 	if (COLS_C == COLS_A && COLS_A == COLS_B && ROWS_C == ROWS_A && ROWS_A == ROWS_B)
 	{
@@ -106,6 +107,8 @@ int main() // 메인함수 시작
 		Matrix_Result2 = (int **)malloc(sizeof(int *) * COLS_A);
 		for (int i = 0; i < COLS_A; i++)
 			Matrix_Result2[i] = (int *)malloc(sizeof(int) * ROWS_A);
+
+		check = 1;
 
 		printf("%s 행렬 ===================\n", oper1);
 		matrix_sub(Matrix_C, Matrix_A, Matrix_Result1, COLS_A);
@@ -120,7 +123,7 @@ int main() // 메인함수 시작
 		printf("\n");
 	}
 	else // 행렬의 크기가 하나라도 다르면 에러메세지 출력
-		printf("행렬의 크기가 맞지 않습니다.");
+		printf("행렬의 크기가 맞지 않습니다.\n"); // 에러메세지 출력
 
 	/* 동적할당한 배열들의 메모리 할당 해제 */
 	for (int i = 0; i < ROWS_A; i++)
@@ -132,12 +135,15 @@ int main() // 메인함수 시작
 	for (int i = 0; i < ROWS_A; i++)
 		free(Matrix_C[i]);
 	free(Matrix_C);
-	for (int i = 0; i < ROWS_B; i++)
-		free(Matrix_Result1[i]);
-	free(Matrix_Result1);
-	for (int i = 0; i < ROWS_B; i++)
-		free(Matrix_Result2[i]);
-	free(Matrix_Result2);
+	if (check == 1)
+	{
+		for (int i = 0; i < ROWS_A; i++)
+			free(Matrix_Result1[i]);
+		free(Matrix_Result1);
+		for (int i = 0; i < ROWS_B; i++)
+			free(Matrix_Result2[i]);
+		free(Matrix_Result2);
+	}
 
 	return 0;
 }
