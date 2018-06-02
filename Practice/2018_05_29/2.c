9e67960220f305d0d98f3e619fe20148f4500af0
alstn2468 <alstn2468_@naver.com>
2018-06-02 23:36:39 +0900

    2018-06-02

--- Practice/2018_05_29/2.c
+++ Practice/2018_05_29/2.c
@@ -4,22 +4,19 @@
  * 학과 : 컴퓨터소프트웨어공학과
  * 프로그램명 : 문제 2
  * 작성일 : 2018 - 05 - 29
- */
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define MAX 100
 #define TRUE 1
 #define FALSE 0
-
-typedef int Data;
 
 int step = 1;
 int stack1_len;
 int stack2_len;
-int stack_temp1[MAX];
-int stack_temp2[MAX];
+
+typedef int Data;
 
 typedef struct _node
 {
@@ -30,16 +27,14 @@ typedef struct _node
 typedef struct _listStack
 {
 	Node *head;
-} ListStack;
-
-typedef ListStack Stack;
+} Stack;
 
 void init(Stack *pstack)
 {
 	pstack->head = NULL;
 }
 
-int is_empty(Stack * pstack)
+int is_empty(Stack *pstack)
 {
 	if (pstack->head == NULL)
 		return TRUE;
@@ -48,134 +43,180 @@ int is_empty(Stack * pstack)
 		return FALSE;
 }
 
-void push(Stack *pstack, int stack_num, Data data)
+void push(Stack *stack1, Stack *stack2, int stack_num, Data data)
 {
-	Stack temp;
-	int i, j;
-
-	Node *newNode = (Node *)malloc(sizeof(Node));
-
-	newNode->data = data;
-	newNode->next = pstack->head;
-
-	pstack->head = newNode;
-
-	printf("---Step %d : 스택%d %d 입력 ---\n", step++, stack_num, data);
-
 	if (stack_num == 1)
-		stack_temp1[++stack1_len] = data;
-
-	if (stack_num == 2)
-		stack_temp2[++stack2_len] = data;
-
-	if (stack1_len > stack2_len)
 	{
-		i = stack1_len;
-		j = stack1_len;
+		Node *newNode = (Node *)malloc(sizeof(Node));
+
+		newNode->data = data;
+		newNode->next = stack1->head;
+
+		stack1->head = newNode;
+		stack1_len++;
 	}
 
 	else
 	{
-		i = stack2_len;
-		j = stack2_len;
+		Node *newNode = (Node *)malloc(sizeof(Node));
+
+		newNode->data = data;
+		newNode->next = stack2->head;
+
+		stack2->head = newNode;
+		stack2_len++;
 	}
-
-	while(TRUE)
-	{
-
-		if (i <= 0 && j <= 0)
-			break;
-
-		if (i > stack1_len)
-			printf("│         │");
-
-		else
-			printf("│ < %3d > │", stack_temp1[i]);
-
-		if(j > stack2_len)
-			printf("│         │\n");
-
-		else
-			printf("│ < %3d > │\n", stack_temp2[j]);
-
-		i--;
-		j--;
-	}
-
-	printf("└─────────┘└─────────┘\n\n");
-
 }
 
-Data pop(Stack *pstack, int stack_num)
+Data pop(Stack *stack1, Stack *stack2, int stack_num)
 {
 	Data rdata;
 	Node *rnode;
-	int i, j;
-
-	if (is_empty(pstack))
-	{
-		printf("Stack Memory Error!");
-		exit(-1);
-	}
-
-	rdata = pstack->head->data;
-	rnode = pstack->head;
-
-	pstack->head = pstack->head->next;
-	free(rnode);
-
-
-	printf("---Step %d : 스택%d %d 삭제 ---\n", step++, stack_num, rdata);
 
 	if (stack_num == 1)
-		stack1_len -= 1;
-
-	if (stack_num == 2)
-		stack2_len -= 1;
-
-	if (stack1_len > stack2_len)
 	{
-		i = stack1_len;
-		j = stack1_len;
+		if (is_empty(stack1))
+		{
+			printf("Stack Memory Error!");
+			exit(-1);
+		}
+
+		rdata = stack1->head->data;
+		rnode = stack1->head;
+
+		stack1->head = stack1->head->next;
+		free(rnode);
+		stack1_len--;
 	}
 
 	else
 	{
-		i = stack2_len;
-		j = stack2_len;
+		if (is_empty(stack2))
+		{
+			printf("Stack Memory Error!");
+			exit(-1);
+		}
+
+		rdata = stack2->head->data;
+		rnode = stack2->head;
+
+		stack2->head = stack2->head->next;
+		stack2_len--;
 	}
-
-	while(TRUE)
-	{
-		if (i <= 0 && j <= 0)
-			break;
-
-		if (i > stack1_len)
-			printf("│         │");
-
-		else
-			printf("│ < %3d > │", stack_temp1[i]);
-
-		if (j > stack2_len)
-			printf("│         │\n");
-
-		else
-			printf("│ < %3d > │\n", stack_temp2[j]);
-
-		i--;
-		j--;
-	}
-
-	printf("└─────────┘└─────────┘\n\n");
 
 	return rdata;
 }
 
+void display_push(Stack *stack1, Stack *stack2, int stack_num, Data data)
+{
+	Node *stack1_temp, *stack2_temp;
+	int stack1_display_count, stack2_display_count;
+
+	stack1_temp = stack1->head;
+	stack2_temp = stack2->head;
+
+	printf("---Step %d : 스택%d %d 입력 ---\n", step++, stack_num, data);
+
+	if (stack1_len > stack2_len)
+	{
+		stack1_display_count = stack1_len;
+		stack2_display_count = stack1_len;
+	}
+
+	else
+	{
+		stack1_display_count = stack2_len;
+		stack2_display_count = stack2_len;
+	}
+
+	while (TRUE)
+	{
+
+		if (stack1_display_count <= 0 && stack2_display_count <= 0)
+			break;
+
+		if (stack1_display_count > stack1_len)
+			printf("│         │");
+
+		else
+		{
+			printf("│ < %3d > │", stack1_temp->data);
+			stack1_temp = stack1_temp->next;
+		}
+
+		if (stack2_display_count > stack2_len)
+			printf("│         │\n");
+
+		else
+		{
+			printf("│ < %3d > │\n", stack2_temp->data);
+			stack2_temp = stack2_temp->next;
+		}
+
+		stack1_display_count--;
+		stack2_display_count--;
+	}
+
+	printf("└─────────┘└─────────┘\n\n");
+}
+
+void display_pop(Stack *stack1, Stack *stack2, int stack_num, int rdata)
+{
+	Node *stack1_temp, *stack2_temp;
+	int stack1_display_count, stack2_display_count;
+
+	stack1_temp = stack1->head;
+	stack2_temp = stack2->head;
+
+	printf("---Step %d : 스택%d %d 삭제 ---\n", step++, stack_num, rdata);
+
+	if (stack1_len > stack2_len)
+	{
+		stack1_display_count = stack1_len;
+		stack2_display_count = stack1_len;
+	}
+
+	else
+	{
+		stack1_display_count = stack2_len;
+		stack2_display_count = stack2_len;
+	}
+
+	while (TRUE)
+	{
+		if (stack1_display_count <= 0 && stack2_display_count <= 0)
+			break;
+
+		if (stack1_display_count > stack1_len)
+			printf("│         │");
+
+		else
+		{
+			printf("│ < %3d > │", stack1_temp->data);
+			stack1_temp = stack1_temp->next;
+		}
+
+		if (stack2_display_count > stack2_len)
+			printf("│         │\n");
+
+		else
+		{
+			printf("│ < %3d > │\n", stack2_temp->data);
+			stack2_temp = stack2_temp->next;
+		}
+
+		stack1_display_count--;
+		stack2_display_count--;
+	}
+
+	printf("└─────────┘└─────────┘\n\n");
+}
 
 int main()
 {
 	Stack s1, s2;
-	int temp, stack_num;
+	Data data;
+	int stack_num, rdata;
 	char oper[5] = { 0 };
 
 	FILE *fp = fopen("data2.txt", "r");
@@ -195,22 +236,26 @@ int main()
 
 		if (!strcmp(oper, "push"))
 		{
-			fscanf(fp, "%d", &temp);
+			fscanf(fp, "%d", &data);
 
 			if(stack_num == 1)
-				push(&s1, stack_num, temp);
+				push(&s1, &s2, stack_num, data);
 
-			if (stack_num == 2)
-				push(&s2, stack_num, temp);
+			else if (stack_num == 2)
+				push(&s1, &s2, stack_num, data);
+
+			display_push(&s1, &s2, stack_num, data);
 		}
 
 		else if (!strcmp(oper, "pop"))
 		{
 			if (stack_num == 1)
-				pop(&s1, stack_num);
+				rdata = pop(&s1, &s2, stack_num);
 
-			if (stack_num == 2)
-				pop(&s2, stack_num);
+			else if (stack_num == 2)
+				rdata = pop(&s1, &s2, stack_num);
+
+			display_pop(&s1, &s2, stack_num, rdata);
 		}
 
 		else
