e1ce2ad1bbd1c6aa493202dcac6e817b2073a224
alstn2468 <alstn2468_@naver.com>
2018-10-18 21:51:36 +0900

    Add Comment BubbleSort.h

--- 2018_10_15_HW4/BubbleSort.h
+++ 2018_10_15_HW4/BubbleSort.h
@@ -4,17 +4,25 @@
 #include <stdio.h> // 표준 입출력 헤더파일 선언
 #include "Swap.h" // 사용자 정의 Swap헤더파일 선언
 
+/**
+ * [bubble_sort 함수]
+ * @param arr [정렬할 배열]
+ * @param n   [데이터의 개수]
+ */
 void bubble_sort(int arr[], int n)
 {
 	int i, j;
-	int temp;
+	// 반복문에서 사용할 변수 선언
 
-	for (i = 0; i < n - 1; i++)
+	/* 배열의 개수만큼 반복 */
+	for (i = n; i > 1; i--)
 	{
-		for (j = 0; j < (n - i) - 1; j++)
+		/* 정렬되지 않은 배열의 데이터 개수만큼 반복 */
+		for (j = 1; j < i; j++)
 		{
-			if (arr[j] > arr[j + 1])
-				swap(&arr[j + 1], &arr[j]);
+			/* j - 1번째 데이터가 j번째 데이터보다 크다면*/
+			if (arr[j - 1] > arr[j])
+				swap(&arr[j - 1], &arr[j]); // 데이터 교환
 		}
 	}
 }
