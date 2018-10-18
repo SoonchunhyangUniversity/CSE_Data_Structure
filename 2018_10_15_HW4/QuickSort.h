d1b90979c28e03291de84924e10441da51bec282
alstn2468 <alstn2468_@naver.com>
2018-10-18 12:49:54 +0900

    Fix QuickSort.h

--- 2018_10_15_HW4/QuickSort.h
+++ 2018_10_15_HW4/QuickSort.h
@@ -1,7 +1,7 @@
 ﻿#ifndef __QUICK__SORT__
 #define __QUICK__SORT__
 
-#include "Swap.h"
+#include "Swap.h" // 사용자 정의 Swap헤더파일 선언
 
 /**
  * [partition 함수]
@@ -21,7 +21,7 @@ int partition(int arr[], int left, int right)
 	pivot = arr[left];
 	// 피봇에 현재 배열의 맨 왼쪽 인덱스 지정
 
-	/* high가 low보다 작으면 종료하는 while문 */
+ 	/* high가 low보다 작으면 종료하는 while문 */
 	do
 	{
 		/* low가 right보다 크거나 배열의 low번째
@@ -30,4 +30,43 @@ int partition(int arr[], int left, int right)
 			low++; // low 1 증가
 		while (low <= right && arr[low] < pivot);
 
-		/* high가 left보다 작거나 배열
+ 		/* high가 left보다 작거나 배열l high번째
+		인덱스 값이 피봇보다 작으면 탈출하는 반복문 */
+		do
+			high--;
+		while (high >= left && arr[high] > pivot);
+
+ 		/* low가 high 보다 클경우 실행 */
+		if (low < high)
+			swap(&arr[low], &arr[high]);
+			// low번째 인덱스와 high번째 인덱스의 데이터 교환
+
+ 	} while (low < high);
+
+ 	swap(&arr[left], &arr[high]);
+	// left번째 데이터와 high번째 데이터 교환
+
+ 	return high; // 맨 오른쪽 인덱스 반환
+}
+
+/**
+ * [quick_sort 함수]
+ * @param arr   [정수형 배열]
+ * @param left  [정렬할 배열의 맨 왼쪽 인덱스]
+ * @param right [정렬할 배열의 맨 오른쪽 인덱스]
+ */
+void quick_sort(int arr[], int left, int right)
+{
+	/* right가 left보다 크면 실행 */
+	if (left <= right)
+	{
+		int pivot = partition(arr, left, right);
+		// 둘로 나눠서
+		quick_sort(arr, left, pivot - 1);
+		// 왼쪽 영역을 정렬
+		quick_sort(arr, pivot + 1, right);
+		// 오른쪽 영역을 정렬
+	}
+}
+
+ #endif
