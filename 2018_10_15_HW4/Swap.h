564635f81d47e72f868f750a86c3fd32d6db16b6
alstn2468 <alstn2468_@naver.com>
2018-10-29 14:32:41 +0900

    Add White Space in Swap.h

--- 2018_10_15_HW4/Swap.h
+++ 2018_10_15_HW4/Swap.h
@@ -2,10 +2,10 @@
 #define __SWAP__
 
 /**
-* [Swap 함수]
-* @param a [배열의 데이터 주소값]
-* @param b [배열의 데이터 주소값]
-*/
+ * [Swap 함수]
+ * @param a [배열의 데이터 주소값]
+ * @param b [배열의 데이터 주소값]
+ */
 void swap(int *a, int *b)
 {
 	int temp = *a;
